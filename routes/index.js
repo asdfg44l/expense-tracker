b78522586af7f27cbecee605779400091ec924f3
Vincent <VLiu1@ftsice.com.tw>
2021-03-15 18:04:32 +0800

    init: setting views

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 //routes
 const home = require('./modules/home')
+const users = require('./modules/users')
 const records = require('./modules/records')
 
 router.use('/', home)
+router.use('/users', users)
 router.use('/records', records)
 
 module.exports = router
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 router.delete('/:id', (req, res) => {
     const record_id = req.params.id
     console.log(record_id)
