2487eee93e15ad57452665c60b91150734830230
Vincent <asdfg10439l@gmail.com>
2021-03-18 17:58:59 +0800

    feat: add iconFinder to get correct record icon

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,44 +1,46 @@
 const express = require('express')
 const router = express.Router()
+
 const Record = require('../../models/record')
-//record category
-const categories = require('../../config/category.json').category
+const Category = require('../../models/category')
 //monthList
 const monthList = require('../../config/monthList.json').month
 
 //util
 const dateTimeFormat = require('../../util/dateTimeFormat')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const userId = req.user._id
     const { category, year, month } = req.query
-    Record.find({ userId })
-        .lean()
-        .then(records => {
-            //searchFilter
+    try {
+        let records = await Record.find({ userId }).lean()
+        let categories = await Category.find().lean()
+        console.log(categories)
+        //searchFilter
 
-            //dateFormat => yyyy-mm-dd 
-            records.forEach(record => {
-                record.date = dateTimeFormat(record.date)
-            })
-            //iconFilter get icon by compare to category
-            // records.forEach(record => {
-            //     record.date = dateTimeFormat(record.date)
-            // })
-            //計算總金額
-            let totalAmount = records.reduce((prev, record) => prev += record.amount, 0)
-
-            return res.render('index', {
-                categories,
-                categoryValue: category,
-                year,
-                monthValue: month,
-                monthList,
-                totalAmount,
-                records
-            })
+        //dateFormat => yyyy-mm-dd 
+        records.forEach(record => {
+            record.date = dateTimeFormat(record.date)
         })
-        .catch(err => console.log(err))
+        //iconFilter get icon by compare to category
+        records.forEach(record => {
+            record.iconName = categories.find(item => item.name === record.category).className
+        })
+        //計算總金額
+        let totalAmount = records.reduce((prev, record) => prev += record.amount, 0)
+
+        return res.render('index', {
+            categories,
+            categoryValue: category,
+            year,
+            monthValue: month,
+            monthList,
+            totalAmount,
+            records
+        })
+    } catch (e) {
+        console.warn(e)
+    }
 
 })
 
